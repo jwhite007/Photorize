cba3abb31ac475e74a14d9088c974380b8af9d4f
James White <jwhite007@comcast.net>
2014-03-18 21:51:49 -0700

    added postgresql database settings to settings.py.  added south to applications in settings.py to manage migrations.

--- photorize/photorize/settings.py
+++ photorize/photorize/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,13 @@ WSGI_APPLICATION = 'photorize.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'photorizedb',
+            'USER': 'photorizer',
+            'PASSWORD': 'photorize',
+            'HOST': 'localhost',
+            # 'ENGINE': 'django.db.backends.sqlite3',
+            # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
