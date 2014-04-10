fd397a2abdbe1491239df9c097c466c19668ec43
James White <jwhite007@comcast.net>
2014-04-10 13:28:27 -0700

    Changes to views.py.  Removed some unnecessary stuff.

--- photorize/photorizer/views.py
+++ photorize/photorizer/views.py
@@ -5,12 +5,15 @@ from django.http import Http404
 # from users.models import users
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from models import Photo, Album
+from models import Photo, Album, Tag
 from django.template import RequestContext, loader
 from django.contrib.auth.models import User
-from forms import LoginForm, RegisterForm
+from forms import LoginForm, CreateAlbumForm, \
+    PhotoForm, AddtoAlbumForm, RemoveFromAlbumForm, \
+    CreateTagForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required, permission_required
+from django import forms
 
 
 def main_view(request):
@@ -19,11 +22,11 @@ def main_view(request):
         print(user_name)
         try:
             albums = (Album.objects.filter(
-                owner__username=user_name))
-            sorted_albums = albums.order_by('-created')
+                owner__username=user_name).order_by('name'))
+            # sorted_albums = albums.order_by('-created')
         except User.DoesNotExist:
             raise Http404
-        context = {'albums': sorted_albums, 'username': user_name}
+        context = {'albums': albums, 'username': user_name}
         return render(request, 'photorizer/albums.html', context)
     else:
         login_form = LoginForm()
@@ -32,16 +35,20 @@ def main_view(request):
 
 
 @login_required
-def photo_view(request, photo):
+def photo_view(request, photo_id):
+    photo = Photo.objects.get(pk=photo_id)
+    if photo.owner_id == request.user.id:
     # user1 = User.objects.get(id=user)
-    back = request.META.get('HTTP_REFERER')
-    photo = Photo.objects.get(id=photo)
-    template = loader.get_template('photorizer/photo.html')
-    context = RequestContext(request, {
-        'photo': photo, 'back': back
-    })
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")
+        back = request.META.get('HTTP_REFERER')
+        photo = Photo.objects.get(id=photo_id)
+        template = loader.get_template('photorizer/photo.html')
+        context = RequestContext(request, {
+            'photo': photo, 'back': back
+        })
+        body = template.render(context)
+        return HttpResponse(body, content_type="text/html")
+    else:
+        return render(request, 'photorizer/permission_denied.html')
 
 # def upload_photo():
 #     pass
@@ -51,31 +58,117 @@ def photo_view(request, photo):
 # @permission_required
 def album_view(request, album_id):
     album = Album.objects.select_related('photos').get(pk=album_id)
+    print("album owner: " + album.owner.username)
+    print("request user: " + request.user.username)
+    print("request user id: " + str(request.user.id))
     context = {'album': album}
-    return render(request, 'photorizer/album.html', context)
+    if album.owner.username == request.user.username:
+        return render(request, 'photorizer/album.html', context)
+    else:
+        return render(request, 'photorizer/permission_denied.html')
 
 
-# @login_required
-# def album_view(request, owner, album_id):
-#     album = Album.objects.select_related('photos').get(pk=album_id)
-#     context = {'album': album}
-#     return render(request, 'photorizer/album.html', context)
-    # user1 = User.objects.get(id=user)
-    # print(user1.username)
-    # album = Album.objects.get(id=1)
-    # photos = album.photos[:]
-    # photos = Photo.objects.filter(album=album)
-    # for photo in photos:
-    #     print(photo)
-    #     print(photo.image)
-    # template = loader.get_template('photorizer/album.html')
-    # context = RequestContext(request, {
-    #     'photos': photos, 'album': album
-    # })
-    # body = template.render(context)
-    # return HttpResponse(body, content_type="text/html")
-    # return HttpResponse('this is the albums page', content_type="text/plain")
+@login_required
+def create_album_view(request):
+    user_id = request.user.id
+    owner = request.user
+    if request.method == 'POST':
+        form = CreateAlbumForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            album = Album(name=name, owner=owner)
+            album.save()
+            return HttpResponseRedirect('/main')
+    else:
+        form = CreateAlbumForm()
+    return render(request, 'photorizer/create_album.html', {'form': form})
 
+
+def delete_album_view(request, album_id):
+    album = Album.objects.get(pk=album_id)
+    album.delete()
+    return HttpResponseRedirect('/main')
+
+
+def delete_photo_view(request, photo_id):
+    photo = Photo.objects.get(pk=photo_id)
+    photo.delete()
+    return HttpResponseRedirect('/photos')
+
+
+def add_photo_view(request):
+    if request.method == 'POST':
+        photo_form = PhotoForm(request.POST, request.FILES)
+        # tag_form = TagForm(request.POST)
+        # if tag_form.is_valid():
+        #     tag_form.save()
+        if photo_form.is_valid():
+            photo = photo_form.save(commit=False)
+            photo.owner = request.user
+            # photo = Photo()
+            # photo.image = form.cleaned_data['image']
+            # photo.name = form.cleaned_data['name']
+            # photo.caption = form.cleaned_data['caption']
+            # photo.tags = form.cleaned_data['tags']
+            photo.save()
+            for tag in photo_form.cleaned_data['tags']:
+                photo.tags.add(tag)
+            photo.save()
+            return HttpResponseRedirect('/photos')
+    else:
+        form = PhotoForm()
+    return render(request, 'photorizer/add_photo.html', {'form': form})
+
+
+def add_photo_to_album_view(request, photo_id):
+    if request.method == 'POST':
+        photo = Photo.objects.get(pk=photo_id)
+        form = AddtoAlbumForm(request.POST)
+        if form.is_valid():
+            for album in form.cleaned_data['albums']:
+                album.photos.add(photo)
+                album.save()
+        return HttpResponseRedirect('/main')
+    else:
+        photo = Photo.objects.get(pk=photo_id)
+        form = AddtoAlbumForm()
+    return render(request, 'photorizer/add_to_album.html', {'form': form, 'photo': photo})
+
+
+def remove_photo_from_album_view(request, photo_id):
+    if request.method == 'POST':
+        photo = Photo.objects.get(pk=photo_id)
+        form = RemoveFromAlbumForm(request.POST)
+        # form.fields['albums'] = forms.ModelChoiceField(Album.objects.filter(photos=photo_id)
+        if form.is_valid():
+            for album in form.cleaned_data['albums']:
+                album.photos.remove(photo)
+                album.save()
+        return HttpResponseRedirect('/main')
+    else:
+        photo = Photo.objects.get(pk=photo_id)
+        form = RemoveFromAlbumForm()
+        form.fields['albums'] = forms.ModelChoiceField(Album.objects.filter(photos=photo_id), widget=forms.CheckboxSelectMultiple())
+    return render(request, 'photorizer/remove_from_album.html', {'form': form, 'photo': photo})
+
+
+def photos_view(request):
+    owner_id = request.user.id
+    photos = Photo.objects.filter(owner_id=owner_id)
+    return render(request, 'photorizer/all_photos.html', {'photos': photos})
+
+
+def add_tag_view(request):
+    if request.method == 'POST':
+        form = CreateTagForm(request.POST)
+        if form.is_valid():
+            tag_name = form.cleaned_data['name']
+            tag = Tag(name=tag_name)
+            tag.save()
+        return redirect('photorizer.views.add_photo_view')
+    else:
+        form = CreateTagForm()
+    return render(request, 'photorizer/add_tag.html', {'form': form})
 
 # def albums(request, user):
 #     # print(user)
