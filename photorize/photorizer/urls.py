b5ddb8291189072261bfe5400cd354fca8472641
James White <jwhite007@comcast.net>
2014-04-10 13:32:17 -0700

    Changes to urls.py.  Added urls for new views added to last commit of views.py.

--- photorize/photorizer/urls.py
+++ photorize/photorizer/urls.py
@@ -7,6 +7,13 @@ urlpatterns = patterns('photorizer.views',  # prefix allows you to refer to view
                        url(r'^photo/(\d+)/$', 'photo_view'),
                        url(r'^main/$', 'main_view', name='homepage'),
                        url(r'^album/(\d+)/$', 'album_view'),
+                       url(r'^album/$', 'create_album_view'),
+                       url(r'^album/(\d+)/delete/$', 'delete_album_view', name='delete_album'),
+                       url(r'^photos/', 'photos_view', name='all_photos'),
+                       url(r'^photo/(\d+)/delete/$', 'delete_photo_view', name='delete_photo'),
+                       url(r'^AddToAlbum/(\d+)/$', 'add_photo_to_album_view', name='add_to_album'),
+                       url(r'^RemoveFromAlbum/(\d+)/$', 'remove_photo_from_album_view', name='remove_from_album'),
+                       url(r'^addTag/$', 'add_tag_view', name='add_tag'),
                        # url(r'^(\w+)album/(\d+)$', 'album_view'),
                        # url(r'^profile/(\d+)/$', 'profile'),
                        # url(r'^accounts/register/$', 'register', name='register'),
@@ -16,7 +23,7 @@ urlpatterns = patterns('photorizer.views',  # prefix allows you to refer to view
                        # url(r'^$', 'stub_view', name="list_photos"),
                        # # can also use: r'^s/(?P<post_id>\d+)/$'
                        # url(r'^photos/(\d+)/$', 'stub_view', name="view_album"),
-                       # url(r'^photo/add/$', 'stub_view', name="add_post"),
+                       url(r'^photo/add/$', 'add_photo_view', name="add_photo"),
                        # url(r'^albums/$', 'albums', name="albums"),
                        )
 urlpatterns += patterns('django.contrib.auth.views',
